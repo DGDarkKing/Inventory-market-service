c6dc22fd2b6b5dd30031d28f59817c90030a6656
Danila Deryavko <deryavkodan@gmail.com>
2024-12-03 01:29:08 +0700

    Updated settings

--- src/settings.py
+++ src/settings.py
@@ -1,38 +1,9 @@
 from pathlib import Path
 
-from pydantic import Field, BaseModel, ConfigDict, field_validator
+from pydantic import Field, BaseModel, ConfigDict, field_validator, UrlConstraints
 from pydantic_core import Url
 from pydantic_settings import BaseSettings
-
-
-class AuthSettings(BaseModel):
-    model_config = ConfigDict(validate_default=True)
-
-    name: str = 'jwt'.upper()
-
-    private_key: str = Field(description='Enter here key or path like string to read key from the file')
-    public_key: str = Field(description='Enter here key or path like string to read key from the file')
-
-    key_id: str = 'hypermarket-auth'
-    key_type: str = 'RSA'
-    algorithm: str = "RS256"
-
-    audience: list[str]
-
-    access_token_lifetime: int = Field(gt=0, default=3600)
-    refresh_token_lifetime: int = Field(gt=0, default=24 * 60 * 60)
-
-    @field_validator(
-        'private_key',
-        'public_key',
-        mode='after'
-    )
-    @classmethod
-    def _validate_keys(cls, data: str) -> str:
-        path = Path(data)
-        if path.exists():
-            data = path.read_text()
-        return data
+from typing_extensions import Annotated
 
 
 class DatabaseSettings(BaseModel):
@@ -45,25 +16,25 @@ class DatabaseSettings(BaseModel):
 
     @property
     def db_async_url(self) -> str:
-        return f'postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}'
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
+
+
+class AmqpSettings(BaseModel):
+    url: Annotated[Url, UrlConstraints(allowed_schemes=["amqp"])]
 
 
 class AppSettings(BaseSettings):
     my_url: Url
-    name: str = "Hypermarket Warehouse Service"
+    name: str = "Hypermarket Inventory service"
     debug: bool = False
-    secret_key: str = Field(min_length=10)
     oidc_discovery_url: str
 
     database: DatabaseSettings
-    auth: AuthSettings
+    amqp: AmqpSettings
 
 
 app_settings = AppSettings(
-    _env_file=(
-        f'{__file__}/../../envs/prod.env',
-        f'{__file__}/../../envs/debug.env'
-    ),
-    _env_file_encoding='utf-8',
-    _env_nested_delimiter='__'
+    _env_file=(f"{__file__}/../../envs/prod.env", f"{__file__}/../../envs/debug.env"),
+    _env_file_encoding="utf-8",
+    _env_nested_delimiter="__",
 )
