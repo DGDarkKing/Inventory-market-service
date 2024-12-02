ed4c39cf3c89356bdcaf13b09e1863039df393fc
Danila Deryavko <deryavkodan@gmail.com>
2024-12-03 01:29:42 +0700

    Removed schemas

--- src/schemas/supply_states.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class SupplyState(str, Enum):
-    ORDERED = "ordered"
-    PENDING = "pending"
-    CANCELED = "canceled"
-    ACCEPTED = "accepted"
-    EXPIRED = "expired"
